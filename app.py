257eb3f7c37a6635fe6c30bc384020a20d67f564
Conor <conor.tighe22@gmail.com>
2018-04-01 19:31:44 +0100

    Sign Language now detected

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ import cv2
 import numpy as np
 import math
 import os
+import numpy as np
+import util as ut
+import svm_train as st 
+import re
 
 
 cap = cv2.VideoCapture(0)
@@ -26,113 +30,61 @@ def index():
 # Camera route
 @app.route("/camera", methods = ['POST'])
 def camera():
-    print("Opening camera!")
+    model=st.trainSVM(17)
+    cap=cv2.VideoCapture(0)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    text= " "
+
+    temp=0
+    previouslabel=None
+    previousText=" "
+    label = None
     while(cap.isOpened()):
-        # read image
-        ret, img = cap.read()
-
-        # get hand data from the rectangle sub window on the screen
+        _,img=cap.read()
         cv2.rectangle(img, (300,300), (100,100), (0,255,0),0)
-        crop_img = img[100:300, 100:300]
+        img1 = img[100:300, 100:300]
+        img_ycrcb = cv2.cvtColor(img1, cv2.COLOR_BGR2YCR_CB)
+        blur = cv2.GaussianBlur(img_ycrcb,(11,11),0)
+        skin_ycrcb_min = np.array((0, 138, 67))
+        skin_ycrcb_max = np.array((255, 173, 133))
+        mask = cv2.inRange(blur, skin_ycrcb_min, skin_ycrcb_max)  # detecting the hand in the bounding box using skin detection
+        imgres,contours,hierarchy = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnt=ut.getMaxContour(contours,4000)			
+        if cnt is not None:
+            gesture,label=ut.getGestureImg(cnt,img1,mask,model)   # passing the trained model for prediction and fetching the result
+            if(label!=None):
+                if(temp==0):
+                    previouslabel=label
+                elif previouslabel==label:
+                    previouslabel=label
+                    temp+=1
+                else:
+                    temp=0
+                if(temp==40):
+                    if(label=='P'):
+                        label=" "
+                        text= text + label
+                    if(label=='Q'):
+                        words = re.split(" +",text)
+                        words.pop()
+                        text = " ".join(words)
+                        #text=previousText
+                print(text)
 
-        # convert to grayscale
-        grey = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
-
-        # applying gaussian blur
-        value = (35, 35)
-        blurred = cv2.GaussianBlur(grey, value, 0)
-
-        # thresholdin: Otsu's Binarization method
-        _, thresh1 = cv2.threshold(blurred, 127, 255,
-                                cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-
-        # show thresholded image
-        cv2.imshow('Thresholded', thresh1)
-
-        # check OpenCV version to avoid unpacking error
-        (version, _, _) = cv2.__version__.split('.')
-
-        if version == '3':
-            image, contours, hierarchy = cv2.findContours(thresh1.copy(), \
-                cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        elif version == '2':
-            contours, hierarchy = cv2.findContours(thresh1.copy(),cv2.RETR_TREE, \
-                cv2.CHAIN_APPROX_NONE)
-
-        # find contour with max area
-        cnt = max(contours, key = lambda x: cv2.contourArea(x))
-
-        # create bounding rectangle around the contour (can skip below two lines)
-        x, y, w, h = cv2.boundingRect(cnt)
-        cv2.rectangle(crop_img, (x, y), (x+w, y+h), (0, 0, 255), 0)
-
-        # finding convex hull
-        hull = cv2.convexHull(cnt)
-
-        # drawing contours
-        drawing = np.zeros(crop_img.shape,np.uint8)
-        cv2.drawContours(drawing, [cnt], 0, (0, 255, 0), 0)
-        cv2.drawContours(drawing, [hull], 0,(0, 0, 255), 0)
-
-        # finding convex hull
-        hull = cv2.convexHull(cnt, returnPoints=False)
-
-        # finding convexity defects
-        defects = cv2.convexityDefects(cnt, hull)
-        count_defects = 0
-        cv2.drawContours(thresh1, contours, -1, (0, 255, 0), 3)
-
-        # applying Cosine Rule to find angle for all defects (between fingers)
-        # with angle > 90 degrees and ignore defects
-        for i in range(defects.shape[0]):
-            s,e,f,d = defects[i,0]
-
-            start = tuple(cnt[s][0])
-            end = tuple(cnt[e][0])
-            far = tuple(cnt[f][0])
-
-            # find length of all sides of triangle
-            a = math.sqrt((end[0] - start[0])**2 + (end[1] - start[1])**2)
-            b = math.sqrt((far[0] - start[0])**2 + (far[1] - start[1])**2)
-            c = math.sqrt((end[0] - far[0])**2 + (end[1] - far[1])**2)
-
-            # apply cosine rule here
-            angle = math.acos((b**2 + c**2 - a**2)/(2*b*c)) * 57
-
-            # ignore angles > 90 and highlight rest with red dots
-            if angle <= 90:
-                count_defects += 1
-                cv2.circle(crop_img, far, 1, [0,0,255], -1)
-            #dist = cv2.pointPolygonTest(cnt,far,True)
-
-            # draw a line from start to end i.e. the convex points (finger tips)
-            # (can skip this part)
-            cv2.line(crop_img,start, end, [0,255,0], 2)
-            #cv2.circle(crop_img,far,5,[0,0,255],-1)
-
-        # define actions required
-        if count_defects == 1:
-            cv2.putText(img,"One", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
-        elif count_defects == 2:
-            str = "Two"
-            cv2.putText(img, str, (5, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
-        elif count_defects == 3:
-            path = 'C:/Users/Iano/Desktop'
-            cv2.imwrite(os.path.join(path , 'img.jpg'), img)
-        elif count_defects == 4:
-            cv2.putText(img,"4", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
-        else:
-            cv2.putText(img,"Five", (50, 50),\
-                        cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
-
-        # show appropriate images in windows
-        cv2.imshow('Gesture', img)
-        all_img = np.hstack((drawing, crop_img))
-        cv2.imshow('Contours', all_img)
-
-        k = cv2.waitKey(10)
+            #cv2.imshow('PredictedGesture',gesture)				  # showing the best match or prediction
+            cv2.putText(img,label,(50,150), font,8,(0,125,155),2)  # displaying the predicted letter on the main screen
+            cv2.putText(img,text,(50,450), font,3,(0,0,255),2)
+            
+        cv2.imshow('Frame',img)
+        #cv2.imshow('Mask',mask)
+        k = 0xFF & cv2.waitKey(10)
         if k == 27:
             break
+
+
+cap.release()        
+cv2.destroyAllWindows()
+
 
 @app.errorhandler(404)
 def page_not_found(e):
